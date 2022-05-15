c0da597f0dc4b06cca7d782a8a5fcea55c20af7a
Vitorzaum336 <vitoraugusto543210@gmail.com>
2022-05-15 12:35:54 -0300

    First commit

---
+++ app/src/main/java/com/example/apprevisao/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.apprevisao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btnEnter).setOnClickListener{
+            comparingNum()
+        }
+    }
+    private fun comparingNum(){
+        try {
+            val result = findViewById<TextView>(R.id.textResult)
+
+            val num1 = findViewById<EditText>(R.id.val1)
+            val num2 = findViewById<EditText>(R.id.val2)
+
+            val valor1 = Integer.parseInt(num1.text.toString())
+            val valor2 = Integer.parseInt(num2.text.toString())
+            when {
+                valor1 > valor2 -> {
+                    result.text = String.format("%d é maior que %d", valor1, valor2)
+                }
+                valor1 < valor2 -> {
+                    result.text = String.format("%d é menor que %d", valor1, valor2)
+                }
+                else -> {
+                    result.text = String.format("%d é igual a %d", valor1, valor2)
+                }
+            }
+        } catch (err: NumberFormatException) {
+            Toast.makeText(
+                this,
+                "É preciso digitar os números",
+                Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+}
